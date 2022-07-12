b45100d711b219249a7af556dc46b7d5a53b3b02
Perkles <gabrielle.leal773@gmail.com>
2022-07-11 22:50:52 -0300

    first commit

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -1,12 +1,16 @@
 package com.dio;
 
+import com.dio.model.Gato;
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a = 2;
-		int b = 3;
-		System.out.println("Hello World!" + (a+b));
+		Livro livro = new Livro();
+		gato gato = new Gato();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		
 	}
 
 }
